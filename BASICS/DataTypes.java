9278f6d8991b481e4f68a358e9590807edea8eb1
Kshitij <Kshitijjha999@gmail.com>
2026-07-07 20:21:02 +0530

    Day 2 - DataTypes in Java

---
+++ BASICS/DataTypes.java
@@ -0,0 +1,28 @@
+public class DataTypes{
+    static void main(String args[]){
+
+        byte num1 = 127;
+        System.out.println(num1);
+
+        short num2 = 32000;
+        System.out.println(num2);
+
+        int num3 = 2147483647;
+        System.out.println(num3);
+
+        long num4 = 9223372036854775807L;
+        System.out.println(num4);
+
+        float num5 = 3.14f;
+        System.out.println(num5);
+
+        double num6 = 3.141592653589793;
+        System.out.println(num6);
+
+        boolean eligibleToVote = true;
+        System.out.println(eligibleToVote);
+
+        char firstCharacter = 'a';
+        System.out.println(firstCharacter);
+    }
+}

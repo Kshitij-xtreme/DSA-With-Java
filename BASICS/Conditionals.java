521fccece82171404685fd881c93148256b7d821
Kshitij <Kshitijjha999@gmail.com>
2026-07-10 10:33:12 +0530

    Day 4 - Conditionals in Java

---
+++ BASICS/Conditionals.java
@@ -0,0 +1,8 @@
+public class Conditionals{
+    public static void main (String args []){
+    int dailyPractice = 12;
+    if (dailyPractice >= 10 ){
+        System.out.println ("Good Consistency");
+    }
+    }
+}
